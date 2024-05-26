6e4be4f0677cf3c189240ec4eaa0bb65f5ee352a
raffgut <rafagutierrez91@hotmail.com>
2024-05-26 08:22:48 -0500

    Se configura proyecto para despliegue

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server started on port: ${PORT}`));
+app.listen(PORT, "0.0.0.0", console.log(`Server started on port: ${PORT}`));
